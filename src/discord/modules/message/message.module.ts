94970064bc857c43ba7922f38b3ad264d25dddb6
MalandiyArtem <malandiyartem@gmail.com>
2024-03-09 15:54:30 +0200

    reactions: implement reacting on message

--- src/discord/modules/message/message.module.ts
+++ src/discord/modules/message/message.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { MessageService } from './message.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ShadowBanEntity } from '../commands/shadow-ban/entities/shadow-ban.entity';
+import { ReactionsEntity } from '../commands/reactions/entities/reactions.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ShadowBanEntity])],
+  imports: [TypeOrmModule.forFeature([ShadowBanEntity, ReactionsEntity])],
   exports: [],
   providers: [MessageService],
 })
--- src/discord/modules/message/message.service.ts
+++ src/discord/modules/message/message.service.ts
@@ -1,9 +1,10 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Context, ContextOf, On } from 'necord';
-import { Message } from 'discord.js';
+import { Client, Message } from 'discord.js';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ShadowBanEntity } from '../commands/shadow-ban/entities/shadow-ban.entity';
 import { Like, Repository } from 'typeorm';
+import { ReactionsEntity } from '../commands/reactions/entities/reactions.entity';
 
 @Injectable()
 export class MessageService {
@@ -12,6 +13,9 @@ export class MessageService {
   constructor(
     @InjectRepository(ShadowBanEntity)
     private readonly shadowBanRepository: Repository<ShadowBanEntity>,
+    @InjectRepository(ReactionsEntity)
+    private readonly reactionsRepository: Repository<ReactionsEntity>,
+    private readonly client: Client,
   ) {}
 
   @On('messageCreate')
@@ -19,6 +23,7 @@ export class MessageService {
     @Context() [message]: ContextOf<'messageCreate'>,
   ) {
     this.shadowBanHandler(message);
+    this.reactionsHandler(message);
   }
 
   private async shadowBanHandler(message: Message) {
@@ -60,6 +65,51 @@ export class MessageService {
       }
     } catch (e) {
       this.logger.error(`Shadow Ban Handler in guild ${message.guildId}: ${e}`);
+    }
+  }
+
+  private async reactionsHandler(message: Message) {
+    try {
+      const authorId = message.author.id;
+      const channelId = message.channel.id;
+      const reactions = await this.reactionsRepository.find({
+        where: {
+          guild: { guildId: message.guild.id },
+        },
+      });
+
+      for (const item of reactions) {
+        if (item.channelIds.length === 0 && item.userIds.length === 0) {
+          await this.setReaction(item.emojis, message);
+          continue;
+        }
+
+        if (
+          item.userIds.includes(authorId) &&
+          item.channelIds.includes(channelId)
+        ) {
+          await this.setReaction(item.emojis, message);
+          continue;
+        }
+
+        if (item.userIds.includes(authorId) && item.channelIds.length === 0) {
+          await this.setReaction(item.emojis, message);
+          continue;
+        }
+
+        if (item.channelIds.includes(channelId) && item.userIds.length === 0) {
+          await this.setReaction(item.emojis, message);
+        }
+      }
+    } catch (e) {
+      this.logger.error(`Set reactions in guild ${message.guildId}: ${e}`);
+    }
+  }
+
+  private async setReaction(idList: string[], message: Message<boolean>) {
+    for (const emojiId of idList) {
+      const emoji = this.client.emojis.cache.get(emojiId) || emojiId;
+      await message.react(emoji);
     }
   }
 }
